e2cd67516ac9e4d80e23e5e89475cd502e258abb
Nguyen Thi Tham <tham27011@gmail.com>
2021-07-24 16:36:36 +0700

    fix conflict

--- app/Console/Commands/RefreshSessionMT5.php
+++ app/Console/Commands/RefreshSessionMT5.php
@@ -70,6 +70,6 @@ class RefreshSessionMT5 extends Command
         $result = json_decode($response->getBody(), true);
         $data['manager_index'] = $result['Result'];
         MT5Connect::where('id', 1)->update($data);
-        DB::connection('markjnee')->select('update mt5_connect set session=' . $data['session'] . ', manager_index=' . $data['manager_index'] . 'where id=1');
+        DB::connection('markjnee')->select('update mt5_connect set session = "' . $data['session'] . '", manager_index = "' . $data['manager_index'] . '" where id=1');
     }
 }
