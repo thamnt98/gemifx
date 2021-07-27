a3128afb4bfc173d5c94c9c9925246257bb1de80
thamnt <thamnt@smartosc.com>
2021-07-27 21:57:28 +0700

    refactor avatart

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -12,7 +12,7 @@
         <div id="userbox" class="userbox">
             <a href="#" data-toggle="dropdown">
                 <figure class="profile-picture">
-                <img src="{{ Auth::user()->copy_of_id ? Auth::user()->copy_of_id : asset('image/!logged-user.jpg') }}" alt="Joseph Doe" class="img-circle" data-lock-picture="assets/images/!logged-user.jpg" />
+                <img src="{{ asset('image/!logged-user.jpg') }}" alt="Joseph Doe" class="img-circle" data-lock-picture="assets/images/!logged-user.jpg" />
                 </figure>
                 <div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
                 <span class="name"><?php echo(Auth::user()->full_name)?> </span>
