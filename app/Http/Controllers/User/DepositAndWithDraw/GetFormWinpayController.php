56d560ab04c3d6900fa27ffb5ca0327e01c3abee
hoangpt <hoangpt@smartosc.com>
2022-07-26 17:59:31 +0700

    Change information bank

--- app/Http/Controllers/User/DepositAndWithDraw/GetFormWinpayController.php
+++ app/Http/Controllers/User/DepositAndWithDraw/GetFormWinpayController.php
@@ -11,12 +11,19 @@ use Illuminate\Support\Str;
 
 class GetFormWinpayController extends Controller
 {
+    const BANK_CODE = 'MB Bank';
+    const USER_CODE = '9704229211928501';
+    const USER_NAME = 'Nguyễn Thị Nhàn';
     public function main(){
         $apiKey = config('deposit.winpay.api_key');
         $endpoint = "https://vwinpay.com/api/info?api_key=" . $apiKey;
         $client = new Client();
         $response = $client->request('GET', $endpoint);
         $result = json_decode($response->getBody());
+        //hard code
+        $result->bank_code = self::BANK_CODE;
+        $result->user_code = self::USER_CODE;
+        $result->user_name = self::USER_NAME;
         $logins = LiveAccount::where('user_id', Auth::user()->id)->pluck('login');
         if ($result->code == 1){
             $code = Str::random('24');
